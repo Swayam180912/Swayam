c967edd0c9e865b4bd5f82b41bae6e185259d97a
Swayam180912 <thakkars@stu.blsd.ca>
2026-05-12 20:23:06 +0000

    commit

---
+++ Convert.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class Convert {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Integer: ");
+        int x = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while (true) {
+            if (x == 1) {
+                list.add(1);
+                break;
+            } else {
+                list.add(x % 2);
+                x /= 2;
+            }
+
+        }
+
+        Collections.reverse(list);
+
+        String answer = "";
+
+        for (int i : list) {
+            answer += Integer.toString(i);
+        }
+        System.out.println(answer);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,34 +1,6 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Collections;
-
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Integer: ");
-        int x = sc.nextInt();
-
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        while (true) {
-            if (x == 1) {
-                list.add(1);
-                break;
-            } else {
-                list.add(x % 2);
-                x /= 2;
-            }
-
-        }
-
-        Collections.reverse(list);
-
-        String answer = "";
-
-        for (int i : list) {
-            answer += Integer.toString(i);
-        }
-        System.out.println(answer);
+    public static void main(String[] args) throws Exception {
+        ProcessBuilder pb = new ProcessBuilder("python", "main.py");
+        Process p = pb.start();
     }
 }
