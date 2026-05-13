cf99c94da148389e47ee2dcb60fe1f9eb60b93d9
Swayam180912 <thakkars@stu.blsd.ca>
2026-05-13 15:13:57 +0000

    commit

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder("python", "main.py");
-        Process p = pb.start();
-    }
-}
