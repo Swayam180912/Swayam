0a9e894b6892e0557cd0a8bf276672609ccc1244
Swayam180912 <thakkars@stu.blsd.ca>
2026-05-13 19:12:27 +0000

    commit

--- InterestCalculator.java
+++ InterestCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class InterestCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Principal Amount: ");
